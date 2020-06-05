c17ca62a17d84c3fd6efa8ed6affe3a245ec10d2
junes7 <2junes@naver.com>
2020-06-05 13:01:50 +0900

    python intro

---
+++ intro/intro_if.py
@@ -0,0 +1,16 @@
+# 조건문
+# 내장함수 -> built-in function
+# 형변환 str -> int
+number = int(input())
+print(number + 3)
+
+# 스트링으로 변환하려면?
+#number = str(number)
+
+# 1. if
+if number > 3 :
+    print("3초과")
+# if구문은 스페이스바 4번 해서 들여쓰기를 한다.
+print("????")
+
+
